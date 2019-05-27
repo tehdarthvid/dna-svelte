80318b7e4afad714420fedf5ece74c3936d27719
Raymund Vidar <darth.vidar@VLAM05190454.local>
2019-05-27 18:48:54 +0800

    Updated rollups, added ESLint & compile-time stuff

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
-import App from './App.svelte';
+import App from "./App.svelte";
 
 const app = new App({
-	target: document.body,
-	props: {
-		name: 'world'
-	}
+  target: document.body,
+  props: {
+    pkg_ver: "<@PKG_VERSION@>",
+    svelte_ver: "<@SVELTE_VERSION@>",
+    git_ver: "<@GIT_COMMIT_NUMBER@>"
+  }
 });
 
 export default app;
