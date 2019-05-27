f41de71aeccc2025f111eeb287307760a0b232a4
tehdarthvid <darthvid@hotmail.com>
2019-05-28 00:11:52 +0800

    need to figure out the difference

--- src/stores/deckStore.js
+++ src/stores/deckStore.js
@@ -1,5 +1,5 @@
 import { readable } from "svelte/store";
-import { initDeck } from "./actions.js";
+import initDeck from "./actions.js";
 
 export const deck = readable(null, function start(set) {
   initDeck(set);
