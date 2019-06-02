db65976ac5505344c07308c8c9b4abe202098e9a
tehdarthvid <darthvid@hotmail.com>
2019-06-03 00:14:16 +0800

    Added modal, but image resizing ain't done yet.

--- src/views/Deck.svelte
+++ src/views/Deck.svelte
@@ -1,6 +1,15 @@
 <script>
   import Card from "../components/Card.svelte";
   import { deck } from "../stores/deckStore.js";
+
+  import Modal from "../components/Modal.svelte";
+  let showModal = false;
+  let urlModal = "";
+
+  function handleModal(event) {
+    //console.log(event.detail.url);
+    urlModal = event.detail.url;
+  }
 </script>
 
 <style scoped>
@@ -17,9 +26,17 @@
   {:else}
     <div class="container">
       {#each $deck as card}
-        <Card {...card} />
+        <Card on:modal={handleModal} {...card} />
       {/each}
     </div>
+  {/if}
+  {#if urlModal}
+    <Modal on:close={() => (urlModal = '')}>
+      <img
+        style="max-width: auto; height: 100%; max-height: 95vh;"
+        src={urlModal} />
+
+    </Modal>
   {/if}
 
 </div>
