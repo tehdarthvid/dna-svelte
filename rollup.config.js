3883e0996ad7a4972f76f8d60877897ff19b07dd
tehdarthvid <darthvid@hotmail.com>
2020-02-10 16:58:50 +0800

    reconfig build output folder

--- rollup.config.js
+++ rollup.config.js
@@ -17,7 +17,7 @@ export default {
     sourcemap: true,
     format: "iife",
     name: "app",
-    file: "public/bundle.js"
+    file: "public/build/bundle.js"
   },
   plugins: [
     replace({
@@ -40,7 +40,7 @@ export default {
       // we'll extract any component CSS out into
       // a separate file — better for performance
       css: css => {
-        css.write("public/bundle.css");
+        css.write("public/build/bundle.css");
       }
     }),
 
