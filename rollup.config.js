5e0dc504218dd6f2957a1a868b2be536a1348b15
tehdarthvid <darthvid@hotmail.com>
2020-03-14 23:07:36 +0800

    updated to newer svelte default template

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,13 @@
 import svelte from "rollup-plugin-svelte";
-import resolve from "rollup-plugin-node-resolve";
-import commonjs from "rollup-plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
 import livereload from "rollup-plugin-livereload";
 import { terser } from "rollup-plugin-terser";
 
-import json from "rollup-plugin-json";
-import replace from "rollup-plugin-replace";
+import replace from "@rollup/plugin-replace";
+
+//import json from "rollup-plugin-json";
+//import replace from "rollup-plugin-replace";
 import { version } from "./package.json";
 const { execSync } = require("child_process");
 
@@ -43,12 +45,11 @@ export default {
         }
       })
     }),
-    json(),
     svelte({
       // enable run-time checks when not in production
       dev: !production,
       // we'll extract any component CSS out into
-      // a separate file — better for performance
+      // a separate file - better for performance
       css: css => {
         css.write("public/build/bundle.css");
       }
@@ -56,11 +57,18 @@ export default {
 
     // If you have external dependencies installed from
     // npm, you'll most likely need these plugins. In
-    // some cases you'll need additional configuration —
+    // some cases you'll need additional configuration -
     // consult the documentation for details:
-    // https://github.com/rollup/rollup-plugin-commonjs
-    resolve(),
+    // https://github.com/rollup/plugins/tree/master/packages/commonjs
+    resolve({
+      browser: true,
+      dedupe: ["svelte"]
+    }),
     commonjs(),
+
+    // In dev mode, call `npm run start` once
+    // the bundle has been generated
+    !production && serve(),
 
     // Watch the `public` directory and refresh the
     // browser on changes when not in production
@@ -74,3 +82,20 @@ export default {
     clearScreen: false
   }
 };
+
+function serve() {
+  let started = false;
+
+  return {
+    writeBundle() {
+      if (!started) {
+        started = true;
+
+        require("child_process").spawn("npm", ["run", "start", "--", "--dev"], {
+          stdio: ["ignore", "inherit", "inherit"],
+          shell: true
+        });
+      }
+    }
+  };
+}
