9015950e92d572e53a5c15e73b4753b5b1eaa55b
tehdarthvid <darthvid@hotmail.com>
2020-05-10 21:44:23 +0800

    chore: Added global styles.

--- src/App.svelte
+++ src/App.svelte
@@ -13,15 +13,14 @@
 
 <style>
   #app {
-    font-family: "Ubuntu", "Noto Sans JP", "Avenir", Helvetica, Arial,
-      sans-serif;
+    /* font-family: "Ubuntu", "Noto Sans JP", "Avenir", Helvetica, Arial,sans-serif; */
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
-    color: #2c3e50;
+    /* color: #2c3e50; */
   }
   #app a {
-    color: #202d3a;
+    /* color: #202d3a; */
     text-decoration: inherit;
     /* no underline */
   }
